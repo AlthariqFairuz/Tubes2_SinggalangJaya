63bec74890ec23827a5757fa65950f6fc6984518
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-05-23 23:10:49 +0700

    fix: coordinate parsing RCC (R = row, C = col).

--- src/logic/src/main/java/com/resources/logic/lib/Coordinate.java
+++ src/logic/src/main/java/com/resources/logic/lib/Coordinate.java
@@ -19,19 +19,35 @@ public class Coordinate {
         return this.col;
     }
 
-    public static Coordinate CodeToCoordinate(String code) {
-        // A1
-        // A2
-        // B1
-        // ...
-        // A -> 0
-        // 1 -> 0
-        // B -> 1
-        // 2 -> 1
+    public static String CoordinateToCode(Coordinate coordinate) {
+        // 0, 0 -> A01
+        // 0, 1 -> A02
+        // 1, 0 -> B01
+        // 1, 1 -> B02
         // ...
 
-        int row = code.charAt(0) - 'A';
-        int col = code.charAt(1) - '1';
+        // Convert row to character
+        char row = (char) ('A' + coordinate.getRow());
+
+        // Convert col to number
+        int col = coordinate.getCol() + 1;
+
+        return String.format("%c%02d", row, col);
+    }
+
+    public static Coordinate CodeToCoordinate(String code) {
+        // A01 -> 0, 0
+        // A02 -> 0, 1
+        // B01 -> 1, 0
+        // B02 -> 1, 1
+        // ...
+
+        // Convert character to row
+        String codeChar = code.substring(0, 1);
+        int row = codeChar.charAt(0) - 'A';
+
+        // Convert number to col
+        int col = Integer.parseInt(code.substring(1)) - 1;
 
         return new Coordinate(row, col);
     }
