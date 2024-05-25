4a61ff68a19354933847c18c50a00e6edc23dfd1
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-05-25 16:32:51 +0700

    feat: fix dependency after merging.

--- src/logic/src/main/java/module-info.java
+++ src/logic/src/main/java/module-info.java
@@ -2,15 +2,11 @@ module com.resources.logic {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+
     requires com.google.gson;
-    requires java.xml;
 
     exports com.resources.logic;
     exports com.resources.logic.product;
-
-    exports com.resources.logic.item;
-    exports com.resources.logic.animal;
-    exports com.resources.logic.plant;
     exports com.resources.logic.plugin;
     exports com.resources.logic.plugin.json;
     exports com.resources.logic.state;
