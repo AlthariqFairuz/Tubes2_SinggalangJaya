7b137e690904c441a7c77dd85cdfeaae96987f8a
randyver <randyverdian99@gmail.com>
2024-05-25 16:17:44 +0700

    feat: add gui and controller winner box

---
+++ src/gui/src/main/java/com/resources/gui/WinnerController.java
@@ -0,0 +1,25 @@
+package com.resources.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import com.resources.logic.Game;
+
+public class WinnerController {
+    @FXML
+    private Label resultLabel;
+
+    public void setWinner(){
+        int scorePlayer1 = Game.getInstance().getPlayer1().getGold();
+        int scorePlayer2 = Game.getInstance().getPlayer2().getGold();
+
+        if(scorePlayer1 > scorePlayer2){
+            resultLabel.setText("The winner is Player 1!");
+        }
+        else if(scorePlayer1 < scorePlayer2){
+            resultLabel.setText("The winner is Player 2!");
+        }
+        else{
+            resultLabel.setText("The game is draw");
+        }
+    }
+}
